59520ba30a1ed98d07fb4f29f974a8b82e67a8aa
KaniCanCode <128730640+NitheeshKani@users.noreply.github.com>
2025-02-12 15:08:51 +0530

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css'
 	import Navbar from '../components/Navbar.svelte'
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
 	let { children } = $props()
 </script>
 
